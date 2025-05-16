9a9cdb07d96860bc1879454bd125459c0a4f20d2
dev4pulse <pulse.developer1@gmail.com>
2025-05-16 13:12:10 -0500

    change to postmappings

--- src/main/java/com/fos/reporting/controller/ReportController.java
+++ src/main/java/com/fos/reporting/controller/ReportController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class ReportController {
         }
     }
 
-    @GetMapping("/product")
+    @PostMapping("/product")
     public ResponseEntity<String> addEntry(@RequestBody @Validated EntryProduct entryProduct) {
         try {
             reportService.addToSales(entryProduct);
