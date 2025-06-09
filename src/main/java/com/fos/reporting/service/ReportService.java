c66c7e6d260b28bf369e879eec95cdfd71aaf140
dev4pulse <pulse.developer1@gmail.com>
2025-06-09 12:56:49 -0400

    Update ReportService.java

--- src/main/java/com/fos/reporting/service/ReportService.java
+++ src/main/java/com/fos/reporting/service/ReportService.java
@@ -1,6 +1,11 @@
 package com.fos.reporting.service;
 
-import com.fos.reporting.domain.*;
+import com.fos.reporting.domain.CollectionsDto;
+import com.fos.reporting.domain.EntryProduct;
+import com.fos.reporting.domain.GetReportRequest;
+import com.fos.reporting.domain.GetReportResponse;
+import com.fos.reporting.domain.Product;
+import com.fos.reporting.domain.ReportData;
 import com.fos.reporting.entity.Collections;
 import com.fos.reporting.entity.Sales;
 import com.fos.reporting.repository.CollectionsRepository;
@@ -15,99 +20,146 @@ import java.util.List;
 
 @Service
 public class ReportService {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss a");
+    // Used for parsing your CollectionsDto and GetReportRequest dates
+    private static final DateTimeFormatter DATE_TIME_FMT =
+        DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     @Autowired
     private SalesRepository salesRepository;
+
     @Autowired
     private CollectionsRepository collectionsRepository;
+
     public boolean addToSales(EntryProduct entryProduct) {
         try {
-        entryProduct.getProducts().forEach(product -> {
-            Sales sales = new Sales();
-            sales.setDateTime(LocalDateTime.parse(entryProduct.getDate(), formatter));
-            sales.setProductName(product.getProductName());
-            sales.setSubProduct(product.getSubProduct());
-            sales.setEmployeeId(entryProduct.getEmployeeId());
-            float opening = product.getOpening();
-            if (product.getOpening() == 0) {
-                Sales recentSale = salesRepository.findTopByProductNameAndSubProductOrderByDateTimeDesc(sales.getProductName(), sales.getSubProduct());
-                if (recentSale != null) {
-                    opening = recentSale.getClosingStock();
-                }
-            }
-            sales.setClosingStock(product.getClosing());
-            sales.setOpeningStock(opening);
-            sales.setTestingTotal(product.getTesting());
-            float sale = product.getClosing() - opening - product.getTesting();
-            sales.setSale(sale);
-            sales.setPrice(product.getPrice());
-            sales.setSaleAmount(sale * product.getPrice());
-            salesRepository.save(sales);
-        });
-            return true;
-        }catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+            // entryProduct.getDate() is a LocalDate; convert to DateTime at start of day
+            LocalDateTime saleDateTime = entryProduct.getDate().atStartOfDay();
 
-    public boolean addToCollections(CollectionsDto collectionsDto) {
-        try {
-            Collections collections = mapToCollections(collectionsDto);
-            LocalDateTime requestedTime = LocalDateTime.parse(collectionsDto.getDate(), formatter);
-            collections.setEmployeeId(collections.getEmployeeId());
-            List<Sales> salesByTime = salesRepository.findByDateTime(requestedTime);
-            double expectedTotal = salesByTime.stream().map(Sales::getSaleAmount)
-                    .mapToDouble(Float::doubleValue).sum();
-            double receivedTotal = collectionsDto.getCashReceived() +
-                    collectionsDto.getPhonePay() +
-                    collectionsDto.getCreditCard() +
-                    collectionsDto.getBorrowedAmount() +
-                    collectionsDto.getDebtRecovered() +
-                    collectionsDto.getExpenses();
-            collections.setExpectedTotal(expectedTotal);
-            collections.setReceivedTotal(receivedTotal);
-            collections.setDifference(expectedTotal - receivedTotal);
-            Collections save = collectionsRepository.save(collections);
-            return save.getId() > 0;
+            for (Product product : entryProduct.getProducts()) {
+                Sales sales = new Sales();
+
+                // 1) Set timestamp
+                sales.setDateTime(saleDateTime);
+
+                // 2) Employee & product info
+                sales.setEmployeeId(entryProduct.getEmployeeId());
+                sales.setProductName(product.getProductName());
+                sales.setSubProduct(product.getSubProduct());
+
+                // 3) Determine openingStock fallback if zero
+                float opening = product.getOpening();
+                if (opening == 0f) {
+                    Sales recent = salesRepository
+                      .findTopByProductNameAndSubProductOrderByDateTimeDesc(
+                          sales.getProductName(),
+                          sales.getSubProduct());
+                    if (recent != null) {
+                        opening = recent.getClosingStock();
+                    }
+                }
+                sales.setOpeningStock(opening);
+
+                // 4) The rest of the fields
+                sales.setClosingStock(product.getClosing());
+                sales.setTestingTotal(product.getTesting());
+                float sale = product.getClosing() - opening - product.getTesting();
+                sales.setSale(sale);
+                sales.setPrice(product.getPrice());
+                sales.setSaleAmount(sale * product.getPrice());
+
+                // 5) Persist
+                salesRepository.save(sales);
+            }
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    private Collections mapToCollections(CollectionsDto collectionsDto) {
-        Collections collections = new Collections();
-        BeanUtils.copyProperties(collectionsDto,collections);
-        collections.setDateTime(LocalDateTime.parse(collectionsDto.getDate(), formatter));
-        return collections;
+    public boolean addToCollections(CollectionsDto dto) {
+        try {
+            Collections coll = mapToCollections(dto);
+
+            LocalDateTime timestamp = LocalDateTime.parse(dto.getDate(), DATE_TIME_FMT);
+            coll.setDateTime(timestamp);
+
+            List<Sales> salesList = salesRepository.findByDateTime(timestamp);
+            double expectedTotal = salesList.stream()
+                .map(Sales::getSaleAmount)
+                .mapToDouble(Float::doubleValue)
+                .sum();
+
+            double receivedTotal = dto.getCashReceived()
+                + dto.getPhonePay()
+                + dto.getCreditCard()
+                + dto.getBorrowedAmount()
+                + dto.getDebtRecovered()
+                + dto.getExpenses();
+
+            coll.setExpectedTotal(expectedTotal);
+            coll.setReceivedTotal(receivedTotal);
+            coll.setDifference(expectedTotal - receivedTotal);
+
+            return collectionsRepository.save(coll).getId() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private Collections mapToCollections(CollectionsDto dto) {
+        Collections c = new Collections();
+        BeanUtils.copyProperties(dto, c);
+        c.setDateTime(LocalDateTime.parse(dto.getDate(), DATE_TIME_FMT));
+        return c;
     }
 
     public GetReportResponse getDashboard(GetReportRequest req) {
-        GetReportResponse getReportResponse = new GetReportResponse();
-        LocalDateTime fromDate = LocalDateTime.parse(req.getFromDate(), formatter);
-        LocalDateTime toDate = LocalDateTime.parse(req.getToDate(), formatter);
-        List<Collections> collections = collectionsRepository.findByDateTimeBetween(fromDate, toDate);
-        List<Sales> sales = salesRepository.findByDateTimeBetween(fromDate, toDate);
-        float petrol = (float) getSalesByProductName(sales, "petrol");
-        float diesel = (float) getSalesByProductName(sales, "diesel");
-        float petrolCollections = (float) getCollections(sales, "petrol");
-        float dieselCollections = (float) getCollections(sales, "diesel");
-        float actualCollections = (float) collections.stream().map(Collections::getReceivedTotal).mapToDouble(x -> x).sum();
-        getReportResponse.setActualCollection(actualCollections);
-        getReportResponse.setDifference(petrolCollections + dieselCollections - actualCollections);
-        getReportResponse.setPetrol(ReportData.builder().saleInLtr(petrol).expectedCollections(petrolCollections).build());
-        getReportResponse.setDiesel(ReportData.builder().saleInLtr(diesel).expectedCollections(dieselCollections).build());
-        return getReportResponse;
-    }
+        GetReportResponse resp = new GetReportResponse();
+        LocalDateTime from = LocalDateTime.parse(req.getFromDate(), DATE_TIME_FMT);
+        LocalDateTime to   = LocalDateTime.parse(req.getToDate(),   DATE_TIME_FMT);
 
-    private static double getSalesByProductName(List<Sales> sales,String productName) {
-        return sales.stream().filter(x -> productName.equalsIgnoreCase(x.getProductName())).
-                map(Sales::getSale).mapToDouble(x -> x).sum();
-    }
-    private static double getCollections(List<Sales> sales,String productName) {
-        return sales.stream().filter(x -> productName.equalsIgnoreCase(x.getProductName())).
-                map(Sales::getSaleAmount).mapToDouble(x -> x).sum();
+        List<Collections> collList = collectionsRepository.findByDateTimeBetween(from, to);
+        List<Sales>       salesList = salesRepository.findByDateTimeBetween(from, to);
+
+        // Total liters sold
+        double petrolLiters = salesList.stream()
+            .filter(s -> "petrol".equalsIgnoreCase(s.getProductName()))
+            .mapToDouble(s -> s.getSale())  // sale = closingStock - openingStock - testingTotal
+            .sum();
+        double dieselLiters = salesList.stream()
+            .filter(s -> "diesel".equalsIgnoreCase(s.getProductName()))
+            .mapToDouble(Sales::getSale)
+            .sum();
+
+        // Expected collections (saleAmount)
+        double petrolColl = salesList.stream()
+            .filter(s -> "petrol".equalsIgnoreCase(s.getProductName()))
+            .mapToDouble(Sales::getSaleAmount)
+            .sum();
+        double dieselColl = salesList.stream()
+            .filter(s -> "diesel".equalsIgnoreCase(s.getProductName()))
+            .mapToDouble(Sales::getSaleAmount)
+            .sum();
+
+        // Actual collected
+        double actual = collList.stream()
+            .mapToDouble(Collections::getReceivedTotal)
+            .sum();
+
+        resp.setActualCollection((float) actual);
+        resp.setDifference((float)(petrolColl + dieselColl - actual));
+
+        resp.setPetrol(ReportData.builder()
+            .saleInLtr((float) petrolLiters)
+            .expectedCollections((float) petrolColl)
+            .build());
+        resp.setDiesel(ReportData.builder()
+            .saleInLtr((float) dieselLiters)
+            .expectedCollections((float) dieselColl)
+            .build());
+
+        return resp;
     }
 }
