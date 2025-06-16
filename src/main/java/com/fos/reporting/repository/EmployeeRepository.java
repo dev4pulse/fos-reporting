abe3f79e58bbd6a317f23d9d5967c40ac90ee490
dev4pulse <pulse.developer1@gmail.com>
2025-06-16 12:17:37 -0500

    Revert "Jagadeesh branch"

--- src/main/java/com/fos/reporting/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fos.reporting.repository;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fos.reporting.entity.Employee;
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByIsActiveTrue();
-}
