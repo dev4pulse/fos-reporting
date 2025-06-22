3f0b44522a15fe58530e04cdf9a7edea65bf25c9
dev4pulse <pulse.developer1@gmail.com>
2025-06-22 11:48:15 -0500

    Update BorrowerRepository.java

--- src/main/java/com/fos/reporting/entity/BorrowerRepository.java
+++ src/main/java/com/fos/reporting/entity/BorrowerRepository.java
@@ -1,7 +1,1 @@
-package com.fos.reporting.repository;
 
-import com.fos.reporting.entity.Borrower;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BorrowerRepository extends JpaRepository<Borrower, Long> {
-}
